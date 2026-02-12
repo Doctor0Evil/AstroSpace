d8cd94f2b64efad8574696a55f00109733a086a4
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-11 19:46:41 -0700

    Update mod.rs

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -9,25 +9,3 @@ pub fn compute_sha256_hash(data: &[u8]) -> String {
     hasher.update(data);
     format!("{:x}", hasher.finalize())
 }
-
-# File: src/utils/math.rs
-use crate::simulation::Lattice;
-
-pub fn check_invariants(lattice: &Lattice) {
-    let total_power: f64 = lattice.sites.iter().map(|s| s.power).sum();
-    let total_church: f64 = lattice.sites.iter().map(|s| s.church).sum();
-    assert!(total_power <= lattice.params.lambda * total_church, "POWER invariant violated");
-
-    for site in &lattice.sites {
-        assert!(site.bioload <= lattice.params.b_max, "Bioload ceiling violated");
-    }
-    // Additional invariants can be added as const assertions or runtime checks
-}
-
-# File: tests/simulation_tests.rs
-#[cfg(test)]
-mod tests {
-    use super::super::simulation::{Lattice, Params, PainMetrics, FearRegime};
-    use approx::assert_relative_eq;
-
-    #[test]
